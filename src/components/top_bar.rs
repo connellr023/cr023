1b254a9d0bf78a5d2aa685d69a616ccd8957b2af
connellr023 <connell.reffo@ucalgary.ca>
2024-02-25 12:25:31 -0700

    Added component module

---
+++ src/components/top_bar.rs
@@ -0,0 +1,10 @@
+use yew::prelude::*;
+
+#[function_component(TopBar)]
+pub fn top_bar() -> Html
+{
+    html!
+    {
+        <h1>{"HI"}</h1>
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
+mod components;
+
 use yew::prelude::*;
 use stylist::Style;
 use stylist::yew::styled_component;
+use components::top_bar::TopBar;
 
 const MAIN_STYLESHEET: &str = include_str!("styles/main.css");
 
 #[styled_component(App)]
-fn app() -> Html {
+fn app() -> Html
+{
     let main_stylesheet: Style = Style::new(MAIN_STYLESHEET).unwrap();
 
-    html! {
+    html!
+    {
         <main class={main_stylesheet}>
-            <h1>{"Test Page!"}</h1>
+            <div class={"app-wrapper"}>
+                <TopBar />
+                <h1>{"Test Page!"}</h1>
+            </div>
         </main>
     }
 }
 
-fn main() {
+fn main()
+{
     yew::start_app::<App>();
 }
