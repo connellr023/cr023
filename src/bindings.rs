396a8f0797592f6501fdc95ea4d67fe2bec792e4
connellr023 <connell.reffo@ucalgary.ca>
2024-02-27 22:54:46 -0700

    Got scroll event listener working

--- src/bindings.rs
+++ src/bindings.rs
@@ -3,6 +3,9 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern
 {
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
+
     #[wasm_bindgen(js_name = setTimeout)]
     pub fn set_timeout(closure: &Closure<dyn FnMut()>, delay: u32) -> f64;
 }
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ mod components;
 mod bindings;
 
 use yew::prelude::*;
-use stylist::Style;
-use stylist::yew::styled_component;
+use stylist::{yew::styled_component, Style};
+use wasm_bindgen::{prelude::Closure, JsCast};
 use components::{name_section::NameSection, gh_img_btn::GithubImageButton};
+use web_sys::{window};
+use crate::bindings::{log};
 
 const MAIN_STYLESHEET: &str = include_str!("styles/main.css");
 
@@ -12,10 +14,27 @@ const MAIN_STYLESHEET: &str = include_str!("styles/main.css");
 fn app() -> Html
 {
     let main_stylesheet: Style = Style::new(MAIN_STYLESHEET).unwrap();
+    let window = window().unwrap();
+
+    //let top_bar_visible = use_state(|| false);
+
+    use_effect(move || {
+        let callback: Closure<dyn FnMut()> = Closure::wrap(Box::new(move || {
+            log("Scrolled");
+        }));
+
+        window.add_event_listener_with_callback("scroll", callback.as_ref().unchecked_ref())
+            .unwrap();
+
+        move || {
+            window.remove_event_listener_with_callback("scroll", callback.as_ref().unchecked_ref())
+                .unwrap();
+        }
+    });
 
     html!
     {
-        <main class={main_stylesheet}>
+        <main id={"app-main"} class={main_stylesheet}>
             <div class={"app-wrapper"}>
                 <GithubImageButton />
                 <NameSection
