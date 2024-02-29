7f6ec00e53e7a6cdbb042eb5eb083374c57f9641
connellr023 <connell.reffo@ucalgary.ca>
2024-02-28 21:16:15 -0700

    Updated scroll event listener

--- src/components/name_section.rs
+++ src/components/name_section.rs
@@ -1,6 +1,6 @@
 use yew::prelude::*;
-use web_sys::{window, Window};
-use wasm_bindgen::{closure::Closure, JsCast};
+use web_sys::{window, Window, Element};
+use wasm_bindgen::{closure::Closure, JsCast, UnwrapThrowExt};
 use stylist::Style;
 use stylist::yew::styled_component;
 use crate::components::typer::Typer;
@@ -21,11 +21,22 @@ pub fn name_section(NameSectionProps { title, message }: &NameSectionProps) -> H
     let stylesheet: Style = Style::new(STYLESHEET).unwrap();
     let window: Window = window().unwrap();
 
-    //let top_bar_visible = use_state(|| false);
-
     use_effect(move || {
+        let element: Element = window
+            .document()
+            .unwrap_throw()
+            .get_element_by_id("name-section-wrapper")
+            .unwrap_throw();
+
+        let window_clone: Window = window.clone();
+
         let callback: Closure<dyn FnMut()> = Closure::wrap(Box::new(move || {
-            log("Scrolled");
+            let window_scroll_y: f64 = window_clone.scroll_y().unwrap_throw();
+            let element_scroll_height: f64 = element.scroll_height().into();
+
+            if window_scroll_y > element_scroll_height {
+                log("Scrolled");
+            }
         }));
 
         window.add_event_listener_with_callback("scroll", callback.as_ref().unchecked_ref())
@@ -39,7 +50,7 @@ pub fn name_section(NameSectionProps { title, message }: &NameSectionProps) -> H
 
     html!
     {
-        <div class={stylesheet}>
+        <div id="name-section-wrapper" class={stylesheet}>
             <h1 class={"title mono"}>{"{"}<a href="https://github.com/connellr023" target="_blank">{title}</a>{"}"}</h1>
             <i class={"message"}><Typer word={message.clone()} interval={110} /></i>
         </div>
